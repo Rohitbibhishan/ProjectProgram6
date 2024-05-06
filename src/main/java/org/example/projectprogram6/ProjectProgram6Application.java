fb0f4a21970698cf0b74883df8b46d5d5e356932
Elite <rohit.raj.mehta723@gmailcom>
2024-05-06 14:19:28 +0530

    added master branch

---
+++ src/main/java/org/example/projectprogram6/ProjectProgram6Application.java
@@ -0,0 +1,13 @@
+package org.example.projectprogram6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProgram6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectProgram6Application.class, args);
+    }
+
+}
